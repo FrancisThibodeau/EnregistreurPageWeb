19f6120dde46e88a93acd5bc0df7ec9f0f67c9e3
MelBeee <mel.beee@deepteal.com>
2015-04-09 22:04:34 -0400

    :end: peut etre :x

--- EnregistreurPageWeb.java
+++ EnregistreurPageWeb.java
@@ -6,74 +6,62 @@ public class EnregistreurPageWeb
 	final static int port = 80;
     public static void main (String args[])
     {
-        if (args.length > 0 && args.length <=2)
+	    if (args.length > 0 && args.length <=2)
         {
-            try
-            {
-                URL url = new URL(args[0]);
-                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-                huc.getResponseCode();
-            }
-            catch (IOException eos)
-            {
-                System.err.println("Adresse URL Invalide");
-                System.exit(1);
-            }
-        }
+			try
+			{
+				if (args.length == 1)
+				{
+					Enregistreur(args[0],"page.html");
+				}
+				else
+				{
+					Enregistreur(args[0],args[1]);
+				}
+			}
+			catch (Exception ioe)
+			{
+				System.err.println(ioe.getMessage());
+			}
+		}
         else
         {
             System.err.println("Nombre de paramètre invalide");
             System.exit(1);
         }
-
-        try
-        {
-            if (args.length == 1)
-            {
-                Enregistreur(args[0],"page.html");
-            }
-            else
-            {
-                Enregistreur(args[0],args[1]);
-            }
-        }
-        catch (Exception ioe)
-        {
-            System.err.println(ioe.getMessage());
-        }
     }
 
     static private void Enregistreur(String WebPage, String sortie)
     {
-		try 
+		try
 		{
 			int numSubstring = 7;
-			if (WebPage.contains("https"))
-				numSubstring = 8;
-			Socket socket = new Socket(InetAddress.getByName(WebPage.substring(numSubstring)), port);
+
+			Socket socket = new Socket(InetAddress.getByName(WebPage), port);
+
 			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
 			PrintWriter fileWriter = 	new PrintWriter(new BufferedWriter(new FileWriter(sortie)));
+		  BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 			writer.println("GET / HTTP/1.1");
-			writer.println("Host: " + WebPage.substring(numSubstring));
-			writer.println("");		
-			writer.flush();
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer.println("Host: " + WebPage);
+			writer.println("");
+            writer.flush();
+
+
 			String result;
-			System.out.println("//////////////" + WebPage.substring(numSubstring) +"//////////////");
-		
+
 			while((result = reader.readLine()) != null)
 			{
-				System.out.println(result);
 				fileWriter.println(result);
-			}		
+			}
 			reader.close();
 			fileWriter.close();
 		}
 		catch (Exception ioex)
 		{
-			System.err.println("Impossible de de connecter a la page");
+			System.err.println("Connexion a la page impossible");
 		}
     }
 }
